fde7e4369d6a80157ffef32fc9a73e8d5b0ed545
vindya <69504272+vindya29@users.noreply.github.com>
2024-08-21 19:13:57 -0500

    Delete Musiqi-main directory

--- Musiqi-main/postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    // Add other PostCSS plugins as needed
-  ],
-};
--- Musiqi-main/src/assets/index.js
+++
@@ -1,7 +0,0 @@
-import loader from './loader.svg';
-import logo from './logo.jpeg';
-
-export {
-  logo,
-  loader,
-};
